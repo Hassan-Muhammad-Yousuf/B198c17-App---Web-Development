39d00d74c74453901c1e799ebb1d895ead21b0c3
Hassan Muhammad Yousuf <alih3766@gmail.com>
2025-02-12 22:08:46 +0100

    adding product page

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+
+admin.site.site_header = "GOBUY"
+admin.site.site_title = "KING ADMIN"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',include('ecom_app.urls')),
     path('ecom_auth/', include('ecom_auth.urls'))
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- ecom/ecom_app/admin.py
+++ ecom/ecom_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from ecom_app.models import Product
 
 # Register your models here.
+admin.site.register(Product)
