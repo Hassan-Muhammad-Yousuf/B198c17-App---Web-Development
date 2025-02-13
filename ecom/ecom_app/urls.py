30fbb5abd57df471c90254568c994e41f2baee0c
Hassan Muhammad Yousuf <alih3766@gmail.com>
2025-02-14 00:32:10 +0100

    Adding purchase Page and checkout Page with form also cart is done

--- ecom/ecom_app/urls.py
+++ ecom/ecom_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from ecom_app import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('purchase', views.purchase,name='purchase'),
+    path('checkout', views.checkout, name = "Checkout"),
 ]
--- ecom/ecom_app/views.py
+++ ecom/ecom_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from ecom_app.models import Product
 from math import ceil
+from ecom_app import keys
 
 # Create your views here.
 def home(request):
+    return render(request, 'index.html')
+
+def purchase(request):
     current_user = request.user
     print(current_user)
     allProds = []
@@ -16,4 +20,11 @@ def home(request):
         allProds.append([prod, range(1, nSlides), nSlides])
 
     params = {'allProds': allProds}
-    return render(request, 'index.html', params)
+    return render(request, 'purchase.html', params)
+
+def checkout(request):
+    if not request.user.is_authenticated:
+        messages.warning(request, "login & Try Again")
+        return redirect('/ecom_auth/login')
+    
+    return render(request, 'checkout.html')
